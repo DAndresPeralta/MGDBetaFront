519973327f9e69f5ba4cbdb15ca752a66b9f532c
dandresperalta <danteandresperalta@gmail.com>
2025-02-18 00:00:04 -0300

    Fix(Store): Add reactive stores from Client

--- src/lib/js/store.js
+++ src/lib/js/store.js
@@ -2,7 +2,9 @@ import { writable } from 'svelte/store';
 
 // -- Estados de componentes
 export const mostrarForm = writable(false);
+export const mostrarFormCliente = writable(false);
 export const mostrarTabla = writable(false);
+export const mostrarTablaCliente = writable(false);
 export const spinner = writable(false);
 export const modalStore = writable({
 	isOpen: false,
@@ -11,7 +13,8 @@ export const modalStore = writable({
 export const toast = writable({ openToast: false, messageToast: '', kind: '' });
 
 // -- Reactividad de arrays
-export const orderStore = writable([]);
+export const orderStore = writable([]); // Es utilizado para renderizar listado de orders en Table
+export const clientStore = writable([]); // Es utilizado para renderizar listado de clientes en TableClient
 
 // -- Métricas
 export const metric1Order = writable({
